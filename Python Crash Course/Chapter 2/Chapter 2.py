827d8495a6be28c479a01de348d410f7fc2d0e07
uhkay <konstantius.kevin@gmail.com>
2024-06-04 15:30:06 +0700

    Finish chapter 2

--- Python Crash Course/Chapter 2/Chapter 2.py
+++ Python Crash Course/Chapter 2/Chapter 2.py
@@ -1,19 +1,80 @@
-''' 2-5. Famous Quote: Find a quote from a famous person you admire. Print the
-quote and the name of its author. Your output should look something like the
-following, including the quotation marks:
-Albert Einstein once said, “A person who never made a mistake never
-tried anything new.” '''
+# 2-1
+
+message = "Python is fun!\n"
+print(message)
+
+# 2-2
+
+message = "Python is fun!\n"
+print(message)
+message = "Python is really fun!\n"
+print(message)
+
+# 2-3
+
+name = "Johan"
+print(f"Hello {name}, would you like to learn some Python today?")
+
+# 2-4
+
+name = "Robert"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+
+# 2-5
 
 print('Albert Einstein once said, "A person who never made a mistake never ' 
 'tried anything new."')
 
-'''2-8. File Extensions: Python has a removesuffix() method that works exactly
-like removeprefix(). Assign the value 'python_notes.txt' to a variable called
-filename. Then use the removesuffix() method to display the filename without
-the file extension, like some file browsers do. '''
+# 2-6
+
+message = "A person who never made a mistake never tried anything new."
+famous_person = "Albert Einstein"
+print(f'{name} once said, "{message}"')
+
+# 2-7
+
+name = "   \tRobert \n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+# 2-8
 
 filename = 'python_notes.txt'
 filename_without_extension = filename.removesuffix('.txt')
 print(filename_without_extension)
+
+# 2-9
+
+print(2 + 6)
+print(9 - 1)
+print(32 / 4)
+print(4 * 2)
+
+# 2-10
+
+favorite_number = 8
+print(f"My favorite number is: {favorite_number}")
+
+# 2-11
+
+# uhkay, 04-06-2024
+#Prints addition, subtraction, division, and multiplication that all results 8.
+print(2 + 6)
+print(9 - 1)
+print(32 / 4)
+print(4 * 2)
+
+# uhkay, 04-06-2024
+# Remove filename extension using removesuffix()
+filename = 'python_notes.txt'
+filename_without_extension = filename.removesuffix('.txt')
+print(filename_without_extension)
+
+# 2-12
 
 import this
