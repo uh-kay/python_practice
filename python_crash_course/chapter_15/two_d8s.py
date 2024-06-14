3c82807b58d14b4c5e46f1151f00893c4ccd5051
uhkay <konstantius.kevin@gmail.com>
2024-06-14 22:00:22 +0700

    Add 15-6

---
+++ python_crash_course/chapter_15/two_d8s.py
@@ -0,0 +1,31 @@
+import plotly.express as px
+
+from die import Die
+
+# Create a D6 and a D10.
+die_1 = Die(8)
+die_2 = Die(8)
+
+# Make some rolls, and store the results in a list.
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+title = "Results of Rolling Two D8 1,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Further customize the chart.
+fig.update_layout(xaxis_dtick=1)
+
+fig.show()
